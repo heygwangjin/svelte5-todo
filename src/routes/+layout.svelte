ddd258c340cadacc5ac9319448188d8a69078d8d
heygwangjin <heygwangjin@gmail.com>
2023-11-18 04:16:44 +0900

    set up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,3 +5,17 @@
 <main>
 	<slot />
 </main>
+
+<style>
+	:global(html, body) {
+		height: 100%;
+	}
+
+	:global(body) {
+		display: grid;
+		place-content: center;
+		font-family: 'Manrope Variable', sans-serif;
+		color: hsl(220 10% 98%);
+		background-color: hsl(220 10% 8%);
+	}
+</style>
